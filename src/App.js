e065149238b84ac90b833d490ed2257fdb6be5ad
wendyyanto <yanto.wendy98@gmail.com>
2020-07-12 16:18:01 +0700

    [feature] setup router and query hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Navbar from 'components/navbar';
-import Footer from 'components/footer';
-import Home from 'components/home';
-import './App.css';
+import React from "react";
+import Navbar from "components/navbar";
+import Footer from "components/footer";
+import Home from "components/home";
+import "./App.css";
 
 const App = () => {
   return (
@@ -12,6 +12,6 @@ const App = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
