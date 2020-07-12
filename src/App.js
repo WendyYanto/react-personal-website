1f52128a426879100499af0d8fb6d17b8b8e9317
wendyyanto <yanto.wendy98@gmail.com>
2020-07-12 15:33:30 +0700

    [feature] setup footer and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from 'components/navbar';
+import Footer from 'components/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Wendy's Website</h1>
+      <Footer />
     </div>
   );
 }
