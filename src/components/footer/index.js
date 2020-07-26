82822db4af042b57869ef54774ca983d94bd59fa
wendyyanto <yanto.wendy98@gmail.com>
2020-07-26 21:47:21 +0700

    [feature] setup social media ref

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,16 @@ const Footer = () => {
     <div className='section contact'>
       <Logo />
       <p>
-        <i class="fa fa-envelope"></i>&nbsp;&nbsp;yanto.wendy98@gmail.com  
+        Contact Me
+      </p>
+      <p>
+        <a href="mailto:yanto.wendy98@gmail.com"><i class="fa fa-envelope"></i></a>
+        &nbsp;&nbsp;
+        <a href="https://www.instagram.com/wendy.yanto/"><i class="fa fa-instagram"></i></a>
+        &nbsp;&nbsp;
+        <a href="https://www.linkedin.com/in/wendyyanto/"><i class="fa fa-linkedin"></i></a>
+        &nbsp;&nbsp;
+        <a href="https://github.com/WendyYanto"><i class="fa fa-github"></i></a>
       </p>
       <p>
         Copyright &copy; 2020 Wendy Yanto
