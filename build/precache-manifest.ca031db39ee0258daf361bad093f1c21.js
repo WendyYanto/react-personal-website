e2c3d38c2a9d971bb1b4581d427c5b3f35d50974
wendyyanto <yanto.wendy98@gmail.com>
2022-06-04 23:46:29 +0800

    fix script

---
+++ build/precache-manifest.ca031db39ee0258daf361bad093f1c21.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eebb93aac52678b03d923b962757d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e10e77a053df59fe586",
+    "url": "/static/css/about-me.2b03dadf.chunk.css"
+  },
+  {
+    "revision": "902a530dd314cf8bcac8",
+    "url": "/static/css/button.adc145e5.chunk.css"
+  },
+  {
+    "revision": "403ec48691c4f3c39ced",
+    "url": "/static/css/cross.6588f0cc.chunk.css"
+  },
+  {
+    "revision": "e45996ef07c9f835ca4f",
+    "url": "/static/css/education-item.70d8918d.chunk.css"
+  },
+  {
+    "revision": "e76715ad720f93846618",
+    "url": "/static/css/education.ac3b60e5.chunk.css"
+  },
+  {
+    "revision": "31246def2db300f37a75",
+    "url": "/static/css/footer.f287423b.chunk.css"
+  },
+  {
+    "revision": "b5ee7a0794eaa40fe08b",
+    "url": "/static/css/hamburger.dcd4cdc0.chunk.css"
+  },
+  {
+    "revision": "87329a3b22ae1a2b305d",
+    "url": "/static/css/header.5b41d60c.chunk.css"
+  },
+  {
+    "revision": "f1d745f226acafbeed07",
+    "url": "/static/css/logo.31a28c21.chunk.css"
+  },
+  {
+    "revision": "a6c4d58cf7c10abad392",
+    "url": "/static/css/main.a6ba8f92.chunk.css"
+  },
+  {
+    "revision": "6a5a33066cbd5ef477f4",
+    "url": "/static/css/navbar-item.1a95cce4.chunk.css"
+  },
+  {
+    "revision": "9a333e703042d6d22b18",
+    "url": "/static/css/navbar.14221797.chunk.css"
+  },
+  {
+    "revision": "c52e254fc1504fe8c4b3",
+    "url": "/static/css/porfolio.5e02bac0.chunk.css"
+  },
+  {
+    "revision": "ad40d6220ef1bf18a479",
+    "url": "/static/css/portfolio-item.e763e921.chunk.css"
+  },
+  {
+    "revision": "2a6653fa4a690fbad798",
+    "url": "/static/css/skill.9ac9c5fa.chunk.css"
+  },
+  {
+    "revision": "8267e026c15aab2af364",
+    "url": "/static/js/0.ab339f05.chunk.js"
+  },
+  {
+    "revision": "63ab52b265d8037b7a859e329f6fecca",
+    "url": "/static/js/0.ab339f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d01eba9fd839f3ba2e",
+    "url": "/static/js/17.cd43401a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/17.cd43401a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e10e77a053df59fe586",
+    "url": "/static/js/about-me.616e69e3.chunk.js"
+  },
+  {
+    "revision": "902a530dd314cf8bcac8",
+    "url": "/static/js/button.9b25b9ec.chunk.js"
+  },
+  {
+    "revision": "403ec48691c4f3c39ced",
+    "url": "/static/js/cross.e8ea71b3.chunk.js"
+  },
+  {
+    "revision": "e45996ef07c9f835ca4f",
+    "url": "/static/js/education-item.1d38f38c.chunk.js"
+  },
+  {
+    "revision": "e76715ad720f93846618",
+    "url": "/static/js/education.85fd3db5.chunk.js"
+  },
+  {
+    "revision": "31246def2db300f37a75",
+    "url": "/static/js/footer.c14b8054.chunk.js"
+  },
+  {
+    "revision": "b5ee7a0794eaa40fe08b",
+    "url": "/static/js/hamburger.e2ebf455.chunk.js"
+  },
+  {
+    "revision": "87329a3b22ae1a2b305d",
+    "url": "/static/js/header.24d6fb8a.chunk.js"
+  },
+  {
+    "revision": "f1d745f226acafbeed07",
+    "url": "/static/js/logo.4b2b8c7c.chunk.js"
+  },
+  {
+    "revision": "a6c4d58cf7c10abad392",
+    "url": "/static/js/main.1aaf770a.chunk.js"
+  },
+  {
+    "revision": "6a5a33066cbd5ef477f4",
+    "url": "/static/js/navbar-item.31ee370e.chunk.js"
+  },
+  {
+    "revision": "9a333e703042d6d22b18",
+    "url": "/static/js/navbar.e001bbb4.chunk.js"
+  },
+  {
+    "revision": "c52e254fc1504fe8c4b3",
+    "url": "/static/js/porfolio.57a7a99d.chunk.js"
+  },
+  {
+    "revision": "ad40d6220ef1bf18a479",
+    "url": "/static/js/portfolio-item.c04ceb0b.chunk.js"
+  },
+  {
+    "revision": "b1fda9547ea1b3670ba9",
+    "url": "/static/js/runtime-main.f2f0a7ce.js"
+  },
+  {
+    "revision": "2a6653fa4a690fbad798",
+    "url": "/static/js/skill.ffd1f32d.chunk.js"
+  },
+  {
+    "revision": "997380ef49184127be06e0211a6d4eae",
+    "url": "/static/media/bg-path.997380ef.png"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/static/media/poppins.ba95810b.ttf"
+  },
+  {
+    "revision": "c4264c65678e0b42a365c7f5f028ab77",
+    "url": "/static/media/wendyyanto.c4264c65.png"
+  }
+]);
